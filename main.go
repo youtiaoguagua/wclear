8cdc7d74ddbd1566f865a3217dc7a6460113c225
youtiaoguagua <2049150434@qq.com>
2022-02-26 23:17:02 +0800

    update

--- main.go
+++ main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/muesli/termenv"
-	"golang.org/x/term"
+	"golang.org/x/sys/windows"
 	"math"
 	"os"
 	"sync"
@@ -14,50 +13,40 @@ var width int
 var height int
 var wg sync.WaitGroup
 
-var lenght = 7
+var length = 7
+
+var mutex sync.RWMutex
 
 func init() {
 	getTerminalSize()
 }
 
-//getTerminalSize get terminal size
-func getTerminalSize() {
-
-	var err error
-
-	width, height, err = term.GetSize(int(os.Stdout.Fd()))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-}
-
 func main() {
-	termenv.HideCursor()
-
-	for i := 0; i <= height; i += 2 * lenght {
+	fmt.Print("\x1b[?25l")
+	for i := 0; i <= height; i += 2 * length {
 		// 向右
 		goRight(i)
 		// 左旋转
-		//leftRotate(i+lenght, width)
+		leftRotate(i+length, width)
 		// 向左
-		goLeft(i + lenght)
+		goLeft(i + length)
 		// 右旋转
-		//rightRotate(i+2*lenght, 0)
+		rightRotate(i+2*length, 0)
 	}
 	fmt.Printf("\x1bc")
-	termenv.ShowCursor()
+	fmt.Print("\x1b[?25h")
+
 }
 
 func leftRotate(x0 int, y0 int) {
-	for i := 180; i <= 360; i += 5 {
+	for i := 180; i <= 360; i += 20 {
 		rotate(x0, y0, i)
 	}
 	wg.Wait()
 }
 
 func rightRotate(x0 int, y0 int) {
-	for i := 180; i >= 0; i -= 5 {
+	for i := 180; i >= 0; i -= 20 {
 		rotate(x0, y0, i)
 	}
 	wg.Wait()
@@ -66,14 +55,14 @@ func rightRotate(x0 int, y0 int) {
 func goRight(i int) {
 	for j := 0; j <= width; j++ {
 		goStraight(i, j)
-		wg.Wait()
 	}
+	wg.Wait()
 }
 func goLeft(i int) {
 	for j := width; j >= 0; j-- {
 		goStraight(i, j)
-		wg.Wait()
 	}
+	wg.Wait()
 }
 
 func calcuCircle(x0 int, y0 int, k int, i int) (float64, float64) {
@@ -83,56 +72,69 @@ func calcuCircle(x0 int, y0 int, k int, i int) (float64, float64) {
 }
 
 func rotate(x0 int, y0 int, i int) {
-	time.Sleep(time.Millisecond * 2)
+	time.Sleep(time.Millisecond * 6)
 	wg.Add(1)
-	go func(x0 int, y0 int) {
-		for k := 0; k < lenght; k++ {
+	go func(x0 int, y0 int, i int) {
+		for k := 0; k < length; k++ {
 			x, y := calcuCircle(x0, y0, k, i)
 			prints(int(math.Floor(x+0.5)), int(math.Floor(y+0.5)), "#")
 		}
 		wg.Add(1)
-		go func() {
-			time.Sleep(time.Millisecond * 3)
-			for k := 0; k < lenght; k++ {
-				x, y := calcuCircle(x0, y0, k, i)
-				prints(int(math.Floor(x+0.5)), int(math.Floor(y+0.5)), " ")
-			}
-			for k := 0; k < lenght; k++ {
-				x, y := calcuCircle(x0, y0, k, i)
-				prints(int(math.Floor(x+0.5)), int(math.Floor(y+0.5)), " ")
-			}
-			wg.Done()
-		}()
+		go cleanRotate(x0, y0, i)
 		wg.Done()
-	}(x0, y0)
+	}(x0, y0, i)
 }
 
 func goStraight(i int, j int) {
-	time.Sleep(time.Millisecond * 3)
+	time.Sleep(time.Millisecond * 4)
 	wg.Add(1)
 	go func(x int, y int) {
-		for k := 0; k < lenght; k++ {
+		for k := 0; k < length; k++ {
 			prints(x+k, y, "#")
 		}
 		wg.Add(1)
-		go func() {
-			time.Sleep(time.Millisecond * 3)
-			for k := 0; k < lenght; k++ {
-				prints(x+k+1, y, " ")
-			}
-			for k := 0; k < lenght; k++ {
-				prints(x+k-1, y, " ")
-			}
-			wg.Done()
-		}()
+		go cleanStraight(x, y)
 		wg.Done()
 	}(i, j)
 }
+
+func cleanRotate(x0 int, y0 int, i int) {
+	time.Sleep(time.Millisecond * 7)
+
+	for k := 0; k < length; k++ {
+		for n := 0; n < 3; n++ {
+			x, y := calcuCircle(x0, y0, k, i)
+			prints(int(math.Floor(x+0.5)), int(math.Floor(y+0.5)), " ")
+		}
+	}
+	wg.Done()
+}
+
+func cleanStraight(x int, y int) {
+	time.Sleep(time.Millisecond * 10)
+
+	for k := 0; k < length; k++ {
+		for n := 0; n < 3; n++ {
+			prints(x+k, y, " ")
+		}
+	}
+	wg.Done()
+}
+
 func prints(x int, y int, str string) {
-	//termenv.MoveCursor(x, y)
+	mutex.Lock()
 	fmt.Printf("\x1b[%d;%dH", x, y)
-	//p := termenv.ColorProfile()
-	//s := termenv.String(str)
-	//s = s.Foreground(p.FromColor(color.RGBA{255, 128, 0, 255}))
 	fmt.Printf(str)
+	mutex.Unlock()
+
+}
+
+//getTerminalSize get terminal size
+func getTerminalSize() {
+	var info windows.ConsoleScreenBufferInfo
+	if err := windows.GetConsoleScreenBufferInfo(windows.Handle(int(os.Stdout.Fd())), &info); err != nil {
+		panic(err)
+	}
+	width = int(info.Window.Right - info.Window.Left + 1)
+	height = int(info.Window.Bottom - info.Window.Top + 1)
 }
